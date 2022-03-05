0d7a9f19849d99e7c8c710fdf05712ee0ad00caa
Vagrant User <vagrant@workspace>
2022-03-05 14:46:06 -0300

    App Engine Provision

--- providers.tf
+++ providers.tf
@@ -15,3 +15,9 @@ provider "google" {
   region  = "us-west1"
   zone    = "us-west1-c"
 }
+
+provider "google-beta" {
+  project = "lab-devops-gcloud"
+  region  = "us-west1"
+  zone    = "us-west1-c"
+}
